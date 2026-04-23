b1aa4e9d40b2bc8b2f865adef477427bcba0c2ed
SofiaLizarazo-a <anas-lizarazoe@unilibre.edu.co>
2026-04-23 18:07:16 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const toggleButtons = document.querySelectorAll('.theme-toggle, .theme-toggle-float');
+
+toggleButtons.forEach(btn => {
+btn.addEventListener('click', () => {
+const currentTheme = document.documentElement.getAttribute('data-theme');
+const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+```
+document.documentElement.setAttribute('data-theme', newTheme);
+localStorage.setItem('theme', newTheme);
+```
+
+});
+});
+
+// Cargar preferencia guardada
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+document.documentElement.setAttribute('data-theme', savedTheme);
+}
